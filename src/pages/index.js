9e4620cf40beab69ff6c1fb6dfdbf6f4f5d711d5
John <john_booker5@mymail.eku.edu>
2020-10-28 14:27:50 -0400

    first commit

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,18 @@
-import React from "react"
+import React from 'react'
+import './style.css';
+import Board from './board.js';
+import Game from './game.js';
 
-export default function Home() {
-  return <div>Hello world!</div>
+
+class GameTest extends React.Component {
+  render() {
+    return (
+      <div className="tictactoe">
+        <h1>TicTacToe {this.props.name}</h1>              	  
+        <Game/>
+      </div>
+    );
+  }
 }
+
+export default GameTest
